d6ce92b3163ec0b8917d4e742a3684f059c5e85b
LiliyaAbdulova <130398lilya@gmail.com>
2018-02-28 23:39:58 +0500

    Добавлены структуры и функции ввода номера и вывода оставшихся в гараже машин

--- main.cpp
+++ main.cpp
@@ -2,4 +2,52 @@
 #include <cstdlib>
 #include <cstring>
 using namespace std;
+struct Car {
+    int num;
+};
+struct Stek
+{
+    Car a;
+    Stek *next;
+};
+void vvod()
+{
+    cin>>x.num;
+}
+void Print()
+{
+    int k=0;
+    Stek *p = u;
+    if(p==0)
+    {
+        cout<<("No cars")<<endl;
+        return;
+    }
+    while(p)
+    {
+        p->a.num;
+        p = p->next;
+        k++;
+    }
+    cout<<("In the garage:")<<endl;
+    p = u;
+    while(p)
+    {
+        cout<<"";
+        cout << p->a.num<<endl;
+        p = p->next;
+    }
+
+}
+void dobavlenie()
+{
+
+}
+
+
+int main()
+{
+
+}
+"# Laba3" 
 "# Laba3" 
