f930943a6d778e609d7bd1cb2bed3dfaa101702f
LiliyaAbdulova <130398lilya@gmail.com>
2018-03-01 00:01:02 +0500

    Добавлена функция для выезжающих из гаража машин

--- main.cpp
+++ main.cpp
@@ -58,7 +58,50 @@ bool Zabiraem(Stek**u, Car &x)
     delete t;
     return true;
 }
+oid vyezjaet_iz_garaja(Stek**u)
+{
+    Stek *v=NULL;
+    Car x;
+    char n[7];
+    cout<<("Leaving car:");
+    cin>>n;
+    while(*u)
+    {
+        if(Zabiraem(u,x))
+        {
+            if(strcmp(n, x.num)==0)
+            {
+                cout<< "Leaved!"<<endl;
+                while(Zabiraem(&v, x)) /// возвращаем элементы из стека V в стек U
+                    dobavlenie(u, x);
+                return;
+            }
+            else
+            {
+                dobavlenie(&v, x);
+            }
+        }
+        else break;
+    }
 
+    cout <<(" No car")<<n<<endl;
+
+    while(Zabiraem(&v,x))
+        dobavlenie (u,x);
+}
+void Clear(Stek **u)
+{
+    if(*u == 0) return;
+    Stek *p = *u;
+    Stek *t;
+    while(p)
+    {
+        t = p;
+        p = p->next;
+        delete t;
+    }
+    *u = 0;
+}
 
 int main()
 {
